c3a3304564a78f9186c3da311fd584e25029eab8
Peter Kagey <peterkagey@gmail.com>
2026-08-28 09:19:59 -0700

    F26 Week 1 updates

--- eleventy.config.js
+++ eleventy.config.js
@@ -61,7 +61,7 @@ export default async function(eleventyConfig) {
 		.addPassthroughCopy("./content/assets/favicon.svg")
 		.addPassthroughCopy({ "node_modules/@mathjax": "vendor/mathjax" })
 		.addPassthroughCopy({"content/assets/_global": "assets"})
-		.addPassthroughCopy("./css/index-2026-05-01.css")
+		.addPassthroughCopy("./css/index-2026-08-26.css")
 		.addPassthroughCopy({"node_modules/prismjs/themes/prism.min.css": "css/prism.css"})
 	;
 	eleventyConfig.addWatchTarget("css/**/*.css");
